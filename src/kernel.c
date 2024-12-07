20afc2955067938345b64ae9f5cac895e88aee95
Jhonattan-rocha <jhonattan246rocha@gmail.com>
2024-12-07 20:03:50 -0300

    Salvando

--- src/kernel.c
+++ src/kernel.c
@@ -5,6 +5,7 @@
 #include <stdint.h>
 #include "memory/heap/kheap.h"
 #include "memory/paging/paging.h"
+#include "disk/disk.h"
 
 uint16_t* video_mem = 0;
 uint16_t terminal_row = 0;
@@ -99,21 +100,11 @@ void kernel_main()
     // Enable paging
     // só pode ativar a paging depois de realizar o switch do chunk reservado para o kernel
 
-    char* ptr = kzalloc(4096); 
-    paging_set(paging_4gb_chunk_get_directory(kernel_chunk), (void*)0x1000, (uint32_t)ptr | PAGING_ACCESS_FROM_ALL | PAGING_IS_PRESENT | PAGING_IS_WRITEABLE);
-
     enable_paging();
     print("Paging initialize success");
 
-    char* ptr2 = (char*) 0x1000;
-    ptr2[0] = 'A';
-    ptr2[1] = 'B';
-
-    ptr[0] = 'B';
-    ptr[1] = 'A';
-    print(ptr2);
-
-    print(ptr);
+    char buf[512];
+    disk_read_sector(0, 1, buf);
 
     enable_interrupts();
     print("Interrupt enable success");
